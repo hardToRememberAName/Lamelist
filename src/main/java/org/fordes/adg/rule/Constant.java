eaf683a8af1220169f90ea6772e696a934a72994
hardToRememberAName <38747597+hardToRememberAName@users.noreply.github.com>
2026-05-26 14:46:10 +0800

    Update Constant.java

--- src/main/java/org/fordes/adg/rule/Constant.java
+++ src/main/java/org/fordes/adg/rule/Constant.java
@@ -8,7 +8,7 @@ public class Constant {
 
     public static final String UPDATE = "# Update time: {}\r\n";
 
-    public static final String REPO = "# Repo URL: AdGuard、AdGuardHome广告过滤规则合并/去重";
+    public static final String REPO = "# Repo URL: AdGuard、AdGuardHome广告过滤规则合并/去重\r\n";
 
     public static final String LOCAL_RULE_SUFFIX = ROOT_PATH + File.separator + "rule";
 
